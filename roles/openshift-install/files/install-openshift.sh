8986e31d1dec035567cd14aecf9b08674779da03
gnunn1 <gerald.b.nunn@gmail.com>
2018-03-30 14:36:52 -0400

    Update for ansible 2.5 and OCP 3.9

--- roles/openshift-install/files/install-openshift.sh
+++ roles/openshift-install/files/install-openshift.sh
@@ -5,5 +5,10 @@ if [ $1 = "origin" ]; then
     ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ~{{amazon_user}}/openshift-ansible/playbooks/byo/config.yml
 else
     # Enterprise
+{% if ocp_version|float <= 3.7  %}
     ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
+{% else %}
+    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
+    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+{% endif %}
 fi
