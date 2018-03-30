880af50e1b1e0c72d44013faba36d4e94e846e22
gnunn1 <gerald.b.nunn@gmail.com>
2018-03-30 19:53:46 -0400

    Fix some bugs

--- roles/openshift-install/files/install-openshift.sh
+++ roles/openshift-install/files/install-openshift.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 export ANSIBLE_HOST_KEY_CHECKING=False
+
+export OA_PREFIX=/usr/share/ansible
+
 if [ $1 = "origin" ]; then
-    # Origin
-    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ~{{amazon_user}}/openshift-ansible/playbooks/byo/config.yml
-else
-    # Enterprise
-{% if ocp_version|float <= 3.7  %}
-    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-{% else %}
-    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
-    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
-{% endif %}
+  OA_PREFIX=/home/{{amazon_user}}
 fi
+
+{% if ocp_version|float <= 3.7  %}
+    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ${OA_PREFIX}/openshift-ansible/playbooks/byo/config.yml
+{% else %}
+    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ${OA_PREFIX}/openshift-ansible/playbooks/prerequisites.yml
+    ansible-playbook -i ~{{amazon_user}}/openshift_inventory.cfg ${OA_PREFIX}/openshift-ansible/playbooks/deploy_cluster.yml
+{% endif %}
